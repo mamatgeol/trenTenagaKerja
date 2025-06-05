04c10de8e7668b116ff041d5769eef4f3757ab26
Auzan Fathi Hawari <100353400+mamatgeol@users.noreply.github.com>
2025-06-05 11:32:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,87 @@
-import streamlit as st
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
-import math
-from pathlib import Path
-
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
-)
-
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
-
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
-
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
-
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
-
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
-
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import month_plot
+from PythonTsa.plot_acf_pacf import acf_pacf_fig
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# In[2]:
 
-st.header(f'GDP in {to_year}', divider='gray')
 
-''
+# Membaca file data
+x = pd.read_csv('femaleaged20-19481981.csv', header=None)
+dates = pd.date_range(start='1948-01', periods=len(x), freq='ME')
+x = pd.Series(x[0].values, index=dates)
 
-cols = st.columns(4)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# In[3]:
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+# Plot data keseluruhan
+x.plot(color='b', linewidth=1.5)
+plt.title("Tren Data Tenaga Kerja Wanita (1948-1981)")
+plt.xlabel("Tahun")
+plt.ylabel("Jumlah Pekerja (dalam ribuan)")
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.show()
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+
+# In[4]:
+
+
+# Plot subset data
+x['1963-01':'1965-12'].plot(marker='o', linestyle='-', color='k')
+plt.title("Subset Data (Jan 1963 - Des 1965)")
+plt.ylabel("Jumlah Pekerja")
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.show()
+
+
+# In[5]:
+
+
+# Plot musiman
+month_plot(x)
+plt.title("Plot Musiman Data Tenaga Kerja Wanita (1948-1981)")
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.show()
+
+
+# In[6]:
+
+
+# Diferensiasi musiman dan biasa
+dDx = sm.tsa.statespace.tools.diff(x, k_diff=1, k_seasonal_diff=1, seasonal_periods=12)
+print(dDx.head())
+
+dDx.plot(color='k', linewidth=1.2)
+plt.title("Diferensiasi Musiman dan Biasa")
+plt.xlabel("Tahun")
+plt.ylabel("Diferensiasi Data")
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.show()
+
+
+# In[7]:
+
+
+# Plot ACF dan PACF
+fig = acf_pacf_fig(dDx.dropna(), both=True, lag=36)
+plt.suptitle("ACF dan PACF runtun diferensiasi musiman", y=0.95)
+plt.tight_layout()
+plt.show()
+
+
+# In[8]:
+
+
+# Uji KPSS
+result = sm.tsa.kpss(dDx.dropna(), regression='c', nlags="auto")
+print(result)
+
